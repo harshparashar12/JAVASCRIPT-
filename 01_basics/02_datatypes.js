6160503efd239c8eda4f7b2b81df58b39ca024c4
harshparashar12 <parasharh53@gmail.com>
2025-06-28 16:01:16 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"Use Strict"; // treat all js code as new version
+
+// alert(3+3) we are using  nodejs , not browser
+
+let name = "harsh";
+let age = 21;
+let isLoggedIn = false
+let state;
+
+
+// number =>2 to power 53
+//bigint
+//string=>""
+// boolean=>true/false
+// null =>standalone value
+// undefined=>
+// symbol =>unique  
+
+
+// object 
+console.log(typeof"harsh"); //String 
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
+
+
